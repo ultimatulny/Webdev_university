207e5fd082399e92694b649d53dd87e27decf72d
Alexander <31167599+ultimatulny@users.noreply.github.com>
2019-04-15 20:32:24 +0500

    fix_bugs

--- JS_5/js/script.js
+++ JS_5/js/script.js
@@ -1,16 +1,21 @@
+var curs_rub = 0.016;
+var curs_euro = 1.13;
+var curs_dollar = 1;
+var curs_funt = 1.31;
+var curs_iena = 0.0089;
+var curs_uan = 0.15;
+var curs_grivna = 0.037;
+
 function go(){
 
 	var from = bank.from.value;
-	var curs = bank.curs.value;
 	var inn = bank.in.value;
 	var sum = bank.summa.value;
 
 
 	var fail = false;
 
-	if(curs == "") fail = "Введите курс!";
 	if(sum == "") fail = "Введите сумму!";
-	if(isNaN(curs)) fail = "Введите число!";
 	if(isNaN(sum)) fail = "Введите число!";
 	if(from == inn) fail = "Вы не можете перевести валюту в саму себя(";
 
@@ -18,22 +23,45 @@ function go(){
 		alert(fail);
 		return false;
 	}
+	var val_from;
+	var val_inn;
+
+	if(from == "Рубли") val_from = curs_rub;
+	if(from == "Евро") val_from = curs_euro;
+	if(from == "Доллар") val_from = curs_dollar;
+	if(from == "Фунт стерлингов") val_from = curs_funt;
+	if(from == "Японская иена") val_from = curs_iena;
+	if(from == "Китайский юань") val_from = curs_uan;
+	if(from == "Украинская гривна") val_from = curs_grivna;
+
+	if(inn == "Рубли") val_inn = curs_rub;
+	if(inn == "Евро") val_inn = curs_euro;
+	if(inn == "Доллар") val_inn = curs_dollar;
+	if(inn == "Фунт стерлингов") val_inn = curs_funt;
+	if(inn == "Японская иена") val_inn = curs_iena;
+	if(inn == "Китайский юань") val_inn = curs_uan;
+	if(inn == "Украинская гривна") val_inn = curs_grivna;
+
+	var curs = val_inn / val_from;
+	var look = val_from / val_inn;
+	document.getElementById("input1").value = look.toFixed(2);
 
 document.getElementById("output").innerHTML = 
 	"Перевод: " + from + "-" + inn +
-	" по курсу: " + curs + "\n" +
+	" по курсу: " + look.toFixed(2) + "\n" +
 	"К выдаче: " + (sum/curs).toFixed(2);
 
 }
 
 function check(){
-	var inp1 = document.getElementById('input1'),
-        inp2 = document.getElementById('input2');
+	var inp2 = document.getElementById('input2');
 
-    document.getElementById('fq').disabled = inp1.value && inp2.value ? false : "disabled";
+    document.getElementById('fq').disabled = inp2.value ? false : "disabled";
+
+
 
 }
 
-function reset(){
+function res(){
 	document.getElementById("output").innerHTML = " ";
 }
